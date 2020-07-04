de3befd520c0da836557eff943b702585f62eb3e
glencbz <glencbz@gmail.com>
2020-07-04 16:24:34 +0800

    Fix height not being inherited by container children

--- frontend/src/components/shared/Container.js
+++ frontend/src/components/shared/Container.js
@@ -14,6 +14,7 @@ window.addEventListener('resize', () => {
 
 const CONTAINER_STYLES = {
   // Mad hacks
+  height: '1px',
   minHeight: 'var(--viewport-height)',
   padding: '5% 5%',
   paddingTop: '3em',
