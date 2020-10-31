74eece08a7a2a34b669f37d541addbeb9488462a
stonefruit <jeromeng888@gmail.com>
2020-10-31 08:13:13 +0800

    Set up validation. Add AllowlistEntry.ts route input validation.

--- backend/src/routes/AllowlistEntry.ts
+++ backend/src/routes/AllowlistEntry.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import express, { Router } from 'express';
+import * as z from 'zod';
 import { requireAdmin } from './middlewares';
 import { handleServiceError } from './transformers';
 import type { AllowlistEntry } from '../services';
+import { logger } from '../config';
+import { stringToNumberTransformer } from './helpers/validation';
 
-function AllowlistEntryRoutes(allowlistService: typeof AllowlistEntry) {
+function AllowlistEntryRoutes(allowlistService: typeof AllowlistEntry): Router {
   const router = express.Router();
 
   router.get('/', requireAdmin, async (req, res) => {
@@ -17,21 +20,46 @@ function AllowlistEntryRoutes(allowlistService: typeof AllowlistEntry) {
 
   router.post('/', requireAdmin, async (req, res) => {
     try {
-      const { phoneNumber, destinationCountry } = req.body;
+      let validatedReq;
+      try {
+        const bodySchema = z.object({
+          phoneNumber: z.string(),
+          destinationCountry: z.string(),
+        });
+        const body = bodySchema.parse(req.body);
+        validatedReq = { body };
+      } catch (error) {
+        logger.error(error);
+        return res.status(400).send(error);
+      }
+
+      const { phoneNumber, destinationCountry } = validatedReq.body;
       const allowlistEntry = await allowlistService.createAllowlistEntry({
         phoneNumber,
         destinationCountry,
       });
-      res.status(200).json(allowlistEntry);
+      return res.status(200).json(allowlistEntry);
     } catch (e) {
-      handleServiceError(e, res);
+      return handleServiceError(e, res);
     }
   });
 
   router.delete('/:id', requireAdmin, async (req, res) => {
-    const { id } = req.params;
-    await allowlistService.deleteAllowlistEntry(Number(id));
-    res.status(200).send();
+    let validatedReq;
+    try {
+      const paramsSchema = z.object({
+        id: stringToNumberTransformer,
+      });
+      const params = paramsSchema.parse(req.params);
+      validatedReq = { params };
+    } catch (error) {
+      logger.error(error);
+      return res.status(400).send(error);
+    }
+
+    const { id } = validatedReq.params;
+    await allowlistService.deleteAllowlistEntry(id);
+    return res.status(200).send();
   });
 
   return router;
