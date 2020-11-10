4bed5be8cc9661e5899bf9d788b377e8dd70bc1b
stonefruit <jeromeng888@gmail.com>
2020-11-10 17:31:31 +0800

    Add input validation for call (#20)

--- backend/src/routes/Call.ts
+++ backend/src/routes/Call.ts
@@ -1,43 +1,58 @@
-import express from 'express';
+import express, { Router } from 'express';
+import * as z from 'zod';
 import { requireSelf } from './middlewares';
 import type {
   Call as CallService,
   TwilioCall as TwilioCallService,
 } from '../services';
 import Stopwatch from '../util/stopwatch';
+import {
+  validateRequest,
+  stringToNumberTransformer,
+} from './helpers/validation';
+
+const GET_SCHEMA = z.object({
+  params: z.object({
+    userId: stringToNumberTransformer,
+  }),
+});
 
 function CallRoutes(
   callService: typeof CallService,
   twilioCallService: typeof TwilioCallService
-) {
+): Router {
   const router = express.Router();
 
-  router.get('/:userId/calls/call-summary', requireSelf, async (req, res) => {
-    const { userId } = req.params;
-    const stopwatch = new Stopwatch();
-    stopwatch.start();
-    const userCalls = await callService.getUserCallsForPeriod(Number(userId));
-    const twilioCalls = await Promise.all(
-      userCalls.map((call) =>
-        twilioCallService.getTwilioCallByParentSid(call.incomingTwilioCallSid)
-      )
-    );
-    const totalDurationSeconds = twilioCalls
-      .map((twilioCall) => {
-        if (!twilioCall) {
-          return 0;
-        }
-        return twilioCall.duration;
-      })
-      .reduce((acc, curr) => acc + curr, 0);
-    stopwatch.stop();
-    req.log.info(
-      'Calculating summary for user %s took %s millis',
-      userId,
-      stopwatch.millisTaken
-    );
-    res.send(String(totalDurationSeconds));
-  });
+  router.get(
+    '/:userId/calls/call-summary',
+    requireSelf,
+    validateRequest(GET_SCHEMA, async (parsedReq, res, req) => {
+      const { userId } = parsedReq.params;
+      const stopwatch = new Stopwatch();
+      stopwatch.start();
+      const userCalls = await callService.getUserCallsForPeriod(userId);
+      const twilioCalls = await Promise.all(
+        userCalls.map((call) =>
+          twilioCallService.getTwilioCallByParentSid(call.incomingTwilioCallSid)
+        )
+      );
+      const totalDurationSeconds = twilioCalls
+        .map((twilioCall) => {
+          if (!twilioCall) {
+            return 0;
+          }
+          return twilioCall.duration;
+        })
+        .reduce((acc, curr) => acc + curr, 0);
+      stopwatch.stop();
+      req.log.info(
+        'Calculating summary for user %s took %s millis',
+        userId,
+        stopwatch.millisTaken
+      );
+      res.send(String(totalDurationSeconds));
+    })
+  );
 
   return router;
 }
