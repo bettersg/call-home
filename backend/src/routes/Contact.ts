b7905bc48205f4f16b04f51bb8b20ac378b65012
stonefruit <jeromeng888@gmail.com>
2020-11-10 11:44:47 +0800

    Add input validation to Contact.ts

--- backend/src/routes/Contact.ts
+++ backend/src/routes/Contact.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Router } from 'express';
+import * as z from 'zod';
 import {
   parseContactRequestBody,
   contactToContactResponse,
@@ -7,43 +8,87 @@ import {
 import { requireSelf } from './middlewares';
 
 import type { Contact } from '../services';
+import {
+  stringToNumberTransformer,
+  validateRequest,
+} from './helpers/validation';
 
-function ContactRoutes(contactService: typeof Contact) {
+const GET_SCHEMA = z.object({
+  params: z.object({
+    userId: stringToNumberTransformer,
+  }),
+});
+
+const POST_SCHEMA = z.object({
+  params: z.object({
+    userId: stringToNumberTransformer,
+  }),
+  body: z.object({
+    name: z.string(),
+    phoneNumber: z.string(),
+    avatar: z.string(),
+  }),
+});
+
+const PUT_SCHEMA = z.object({
+  params: z.object({
+    userId: stringToNumberTransformer,
+    contactId: stringToNumberTransformer,
+  }),
+  body: z.object({
+    name: z.string().optional(),
+    phoneNumber: z.string().optional(),
+    avatar: z.string().optional(),
+  }),
+});
+
+const DELETE_SCHEMA = z.object({
+  params: z.object({
+    userId: stringToNumberTransformer,
+    contactId: stringToNumberTransformer,
+  }),
+});
+
+function ContactRoutes(contactService: typeof Contact): Router {
   const router = express.Router();
 
-  router.get('/:userId/contacts/', requireSelf, async (req, res) => {
-    const { userId } = req.params;
-    const contacts = await contactService.listContactsByUserId(Number(userId));
-    return res.status(200).json(contacts.map(contactToContactResponse));
-  });
+  router.get(
+    '/:userId/contacts/',
+    requireSelf,
+    validateRequest(GET_SCHEMA, async (parsedReq, res) => {
+      const { userId } = parsedReq.params;
+      const contacts = await contactService.listContactsByUserId(userId);
+      return res.status(200).json(contacts.map(contactToContactResponse));
+    })
+  );
 
   router.post(
     '/:userId/contacts/',
     requireSelf,
     parseContactRequestBody,
-    async (req, res) => {
-      const { userId } = req.params;
-      const contact = req.body;
+    validateRequest(POST_SCHEMA, async (parsedReq, res, req) => {
+      const { userId } = parsedReq.params;
+      const contact = parsedReq.body;
       try {
         req.log.info('CREATING', userId, contact);
         const savedContact = await contactService.createContact(
-          Number(userId),
+          userId,
           contact
         );
         res.status(200).json(contactToContactResponse(savedContact));
       } catch (e) {
         handleServiceError(e, res);
       }
-    }
+    })
   );
 
   router.put(
     '/:userId/contacts/:contactId',
     requireSelf,
     parseContactRequestBody,
-    async (req, res) => {
-      const { userId, contactId } = req.params;
-      const contact = req.body;
+    validateRequest(PUT_SCHEMA, async (parsedReq, res) => {
+      const { userId, contactId } = parsedReq.params;
+      const contact = parsedReq.body;
       try {
         const savedContact = await contactService.updateContact(
           userId,
@@ -54,17 +99,17 @@ function ContactRoutes(contactService: typeof Contact) {
       } catch (e) {
         handleServiceError(e, res);
       }
-    }
+    })
   );
 
   router.delete(
     '/:userId/contacts/:contactId',
     requireSelf,
-    async (req, res) => {
-      const { userId, contactId } = req.params;
+    validateRequest(DELETE_SCHEMA, async (parsedReq, res) => {
+      const { userId, contactId } = parsedReq.params;
       await contactService.deleteContact(Number(userId), contactId);
       res.status(200).send();
-    }
+    })
   );
   return router;
 }
