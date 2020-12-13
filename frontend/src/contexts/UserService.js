eab88a7f7373bfef7c7884eb43b27a5d38cc0129
glencbz <glencbz@gmail.com>
2020-12-13 19:02:05 +0800

    Remove extra UserService.js

--- frontend/src/contexts/UserService.js
+++
@@ -1,9 +0,0 @@
-import createServiceProvider from './createServiceProvider';
-import { User } from '../services';
-
-const {
-  Provider: UserServiceProvider,
-  hook: useUserService,
-} = createServiceProvider(User);
-
-export { UserServiceProvider, useUserService };
