aab99038d2e6ba652d2c0a5607034926460aaed8
glencbz <glencbz@gmail.com>
2021-01-30 19:06:15 +0800

    WIP Prove QR code display logic

--- frontend/src/scenes/AdminPage/RedeemableCodeTabContent.tsx
+++ frontend/src/scenes/AdminPage/RedeemableCodeTabContent.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState, FormEvent } from 'react';
+import QRCode from 'qrcode.react';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Dialog from '@material-ui/core/Dialog';
 import Table from '@material-ui/core/Table';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
@@ -10,12 +13,53 @@ import TableCell from '@material-ui/core/TableCell';
 import Typography from '@material-ui/core/Typography';
 import CropFreeIcon from '@material-ui/icons/CropFree';
 import { useRedeemableCodeService } from 'contexts';
+import './RedeemableCodeTabContent.css';
+
+const baseUrl = process.env.PUBLIC_URL || 'http://localhost:4000';
+
+function QRCodeDialog({
+  open,
+  code,
+  onClose,
+}: {
+  open: boolean;
+  code: string;
+  onClose: () => void;
+}) {
+  const codeUrl = `${baseUrl}/wip-placeholder/?code=${code}`;
+  return (
+    <Dialog
+      classes={{
+        root: 'opaque-dialog',
+      }}
+      fullWidth
+      open={open}
+      onClose={onClose}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '2rem',
+        }}
+      >
+        <IconButton style={{ marginBottom: '1rem' }} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+        <QRCode size={256} value={codeUrl} />
+      </div>
+    </Dialog>
+  );
+}
 
 export default function RedeemableCodeTabContent() {
   const [
     redeemableCodeState,
     redeemableCodeService,
   ] = useRedeemableCodeService();
+  const [displayedQrCode, setDisplayedQrCode] = useState('');
+
   const redeemableCodes = redeemableCodeState?.codes ?? [];
 
   useEffect(() => {
@@ -24,9 +68,8 @@ export default function RedeemableCodeTabContent() {
     }
   }, [redeemableCodeService]);
 
-  const createFacebookDormCode = () => {
+  const createFacebookDormCode = () =>
     redeemableCodeService?.createRedeemableCode();
-  };
 
   return (
     <>
@@ -55,7 +98,10 @@ export default function RedeemableCodeTabContent() {
               <TableRow key={redeemableCode.id}>
                 <TableCell>{redeemableCode.code}</TableCell>
                 <TableCell>
-                  <IconButton role="button">
+                  <IconButton
+                    role="button"
+                    onClick={() => setDisplayedQrCode(redeemableCode.code)}
+                  >
                     <CropFreeIcon />
                   </IconButton>
                 </TableCell>
@@ -64,6 +110,11 @@ export default function RedeemableCodeTabContent() {
           </TableBody>
         </Table>
       </TableContainer>
+      <QRCodeDialog
+        open={Boolean(displayedQrCode)}
+        code={displayedQrCode}
+        onClose={() => setDisplayedQrCode('')}
+      />
     </>
   );
 }
