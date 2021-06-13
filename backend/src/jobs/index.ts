8bfc3004b6603999123d9bdfe9e205d8f0e4c81f
Glen <glen@Glens-MacBook-Pro.local>
2021-06-13 13:43:28 +0800

    Add feature flag to disable periodic jobs
    Having two sets of periodic jobs on staging and production seems like a
    plausible cause for data inconsistency. This allows periodic jobs to be disabled
    on staging so that only one set of jobs is running.

--- backend/src/jobs/index.ts
+++ backend/src/jobs/index.ts
@@ -1,11 +1,14 @@
 import { logger } from '../config';
-import { TwilioCall } from '../services';
+import { Feature, TwilioCall } from '../services';
 import refreshConnectedCalls from './refreshConnectedCalls';
 import refreshNoPriceCalls from './refreshNoPriceCalls';
 import refreshJoinedCalls from './refreshJoinedCalls';
 
-logger.info('Initializing jobs');
-
-refreshConnectedCalls(TwilioCall);
-refreshNoPriceCalls(TwilioCall);
-refreshJoinedCalls();
+if (!Feature.shouldDisablePeriodicJobs()) {
+  logger.info('Initializing jobs');
+  refreshConnectedCalls(TwilioCall);
+  refreshNoPriceCalls(TwilioCall);
+  refreshJoinedCalls();
+} else {
+  logger.info('Periodic jobs disabled');
+}
--- backend/src/services/Feature.ts
+++ backend/src/services/Feature.ts
@@ -2,6 +2,7 @@ const {
   ENABLE_ALLOWLIST_SMS,
   DISABLE_ALLOWLIST,
   ENABLE_DORM_VALIDATION,
+  DISABLE_PERIODIC_JOBS,
 } = process.env;
 
 function shouldEnableAllowlistSms(): boolean {
@@ -16,10 +17,16 @@ function shouldEnableDormValidation(): boolean {
   return Boolean(ENABLE_DORM_VALIDATION);
 }
 
+// Allows periodic jobs to be disabled. Might be necessary to prevent multiple instances from writing to the same database.
+function shouldDisablePeriodicJobs(): boolean {
+  return Boolean(DISABLE_PERIODIC_JOBS);
+}
+
 type PeriodicCreditCohort = 'week' | 'month';
 
 export {
   shouldDisableAllowlist,
   shouldEnableAllowlistSms,
   shouldEnableDormValidation,
+  shouldDisablePeriodicJobs,
 };
