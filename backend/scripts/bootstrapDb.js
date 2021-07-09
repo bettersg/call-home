1b1203bf0b7904e79d23ce21fb17405c12e7599c
Glen <glen@Glens-MacBook-Pro.local>
2021-07-09 16:21:12 +0800

    Fix backend Dockerfile.dev
    - Rename backend/Dockerfile to backend/Dockerfile.dev
    - Add steps that copy over sequelize config and bootstrap the db
      properly for seeding.
    - Upgrade Typescript to 4.2 to accommodate Cheerio.
    - Modify sequelize config to disable SSL in development
    - Set NODE_ENV in docker-compose
    - Add dockerize script to `shared/package.json`. May be useful to
      build the base image beyond just docker compose.

---
+++ backend/scripts/bootstrapDb.js
@@ -0,0 +1,3 @@
+// All this does is import the sequelize files.
+// When the sequelize files are imported, sequelize immediately knows how to bootstrap the db with the exact configuration expected by the models.
+require('../src/models/index.ts');
