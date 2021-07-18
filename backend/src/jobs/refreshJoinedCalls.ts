dff0740d213e18b46c6939d9306f3ba5d2521fff
Glen <glen@Glens-MacBook-Pro.local>
2021-07-18 18:29:46 +0800

    Fix minor type issues and document setupProxy.js

--- backend/src/jobs/refreshJoinedCalls.ts
+++ backend/src/jobs/refreshJoinedCalls.ts
@@ -2,9 +2,9 @@
 // This ensures that the view is refreshed.
 // TODO document the view in code and possibly find another way to manage analytics data.
 
+import { readFile } from 'fs';
 import { logger } from '../config';
 import { sequelize } from '../models';
-import { readFile } from 'fs';
 
 const jobIntervalMillis = 24 * 60 * 60 * 1000;
 
@@ -12,7 +12,7 @@ function refreshJoinedCalls() {
   async function job() {
     try {
       logger.info('refreshJoinedCalls==========');
-      await readFile('join_calls.sql', 'utf8', (_, sql) => sequelize.query(sql));
+      readFile('join_calls.sql', 'utf8', (_, sql) => sequelize.query(sql));
     } catch (error) {
       logger.error(error);
     } finally {
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
+
+// This is not shipped to the frontend. This is pure create-react-app magic meant for development.
+// However this is in the src/ directory, which means it's treated like a frontend file.
+// As such, we have to disable the eslint rules that don't apply to this file.
+
+// This is installed as a devDependency.
 const createProxyMiddleware = require('http-proxy-middleware');
 
 const { BACKEND_HOST = 'localhost' } = process.env;
 
-module.exports = function (app) {
+function setupProxy(app) {
   app.use(
     '/oauth',
     createProxyMiddleware({
@@ -10,4 +18,6 @@ module.exports = function (app) {
       changeOrigin: true,
     })
   );
-};
+}
+
+module.exports = setupProxy;
