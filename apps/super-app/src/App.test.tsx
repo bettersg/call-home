c75c5c2dd6950d21bcbb8f07f763cefefd0645db
Glen <glencbz@gmail.com>
2022-12-14 15:23:00 -0800

    super-app: add react-router
    Add a simple `react-router-dom` setup by moving the testing content from
    `App.tsx` to a `/scratch` route. We can reuse this route for whatever
    other content we want to test out.
    
    This uses the old component-based routing instead of the react-router v6
    `createXRouter` APIs. This is largely because this makes it easier to
    use a different router in testing vs in production (the "browser router"
    isn't available in testing; the "memory router" is recommended instead).
    The <App/> component is wrapped by the <(Browser|Memory)Router/>
    component, and we can easily change routers by specifying the right
    wrapping router in the right environment.
    
    I'm not sure if having the router outside of the app is actually a good
    pattern (react-router seems to recommend this) or more of a hack (we now
    have some providers inside the <App/> and some outside of it, which
    feels inconsistent), but we can revisit this pattern later.

--- apps/super-app/src/App.test.tsx
+++ apps/super-app/src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders some nonsense buttons', () => {
-  render(<App />);
+test('renders a nonsense homepage', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const todoLink = screen.getByText(/TODO/i);
+  expect(todoLink).toBeInTheDocument();
+});
+
+test('renders some nonsense buttons on /scratch', () => {
+  render(
+    <MemoryRouter initialEntries={['/scratch']}>
+      <App />
+    </MemoryRouter>
+  );
+
   const primaryButton = screen.getByText(/Primary/i);
   expect(primaryButton).toBeInTheDocument();
   const neutralButton = screen.getByText(/Neutral/i);
--- apps/super-app/src/App.tsx
+++ apps/super-app/src/App.tsx
@@ -2,29 +2,16 @@ import './App.css';
 import { useEffect } from 'react';
 import { consoleSmokeTest } from './services';
 import { ThemeProvider } from './common/contexts';
-import { Container } from './common/components';
-import {
-  PrimaryButton,
-  NeutralButton,
-  ErrorButton,
-} from './common/components/RoundedButton';
+import { AppRoutes } from './routes/router';
 
 function App() {
   useEffect(() => {
     consoleSmokeTest();
   }, []);
+
   return (
     <ThemeProvider>
-      <Container
-        style={{
-          background: 'no-repeat url(/images/background.svg) bottom center',
-          backgroundSize: 'contain',
-        }}
-      >
-        <PrimaryButton>Primary</PrimaryButton>
-        <NeutralButton>Neutral</NeutralButton>
-        <ErrorButton>Error</ErrorButton>
-      </Container>
+      <AppRoutes></AppRoutes>
     </ThemeProvider>
   );
 }
