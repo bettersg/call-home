61d4226c50aacc9bd16cfe46c89f77cff2578f36
Glen <glencbz@gmail.com>
2022-12-14 13:56:47 -0800

    super-app: add background to container

--- apps/super-app/src/App.tsx
+++ apps/super-app/src/App.tsx
@@ -8,7 +8,12 @@ import { Container } from './common/components';
 function App() {
   useEffect(() => {consoleSmokeTest()}, []);
   return (<ThemeProvider>
-      <Container>
+      <Container
+        style={{
+          background: 'no-repeat url(/images/background.svg) bottom center',
+          backgroundSize: 'contain',
+        }}
+      >
         <PrimaryButton>Primary</PrimaryButton>
         <NeutralButton>Neutral</NeutralButton>
         <ErrorButton>Error</ErrorButton>
