526cf1b8dd522384b25d23c4d8080e6879b737bd
Glen <glencbz@gmail.com>
2022-12-14 11:31:36 -0800

    super-app: add and use Container
    Wrap App.tsx in a <Container>, which centers the elements horizontally,
    stacks them vertically, and takes up 100% of the viewport height

---
+++ apps/super-app/src/common/components/Container.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import MuiContainer from '@mui/material/Container';
+
+// TODO document why meddling with --viewport-height is needed
+document.documentElement.style.setProperty(
+  '--viewport-height',
+  `${window.innerHeight}px`
+);
+window.addEventListener('resize', () => {
+  document.documentElement.style.setProperty(
+    '--viewport-height',
+    `${window.innerHeight}px`
+  );
+});
+
+const CONTAINER_STYLES: React.CSSProperties = {
+  // Mad hacks
+  height: '1px',
+  minHeight: 'var(--viewport-height)',
+  padding: '2rem',
+  paddingTop: '3rem',
+  display: 'flex',
+  flexDirection: 'column',
+  position: 'relative',
+};
+
+export default function Container({
+  children,
+  style,
+  ...rest
+}: {
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+}) {
+  const containerStyles = style
+    ? {
+        ...CONTAINER_STYLES,
+        ...style,
+      }
+    : CONTAINER_STYLES;
+  return (
+    <MuiContainer style={containerStyles} {...rest} maxWidth="sm">
+      {
+        children as any // Needed because the props don't accept null | undefined
+      }
+    </MuiContainer>
+  );
+}
