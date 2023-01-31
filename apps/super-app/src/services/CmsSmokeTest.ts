2c27941d0fbed3104832f2efcbbcd94af9bf0a69
Glen <glencbz@gmail.com>
2023-01-31 17:32:17 +0800

    super-app: add Strapi i18n smoke test
    Enable the i18n Strapi plugin and demonstrate it with another smoke
    test. As before, the goal is mainly to demonstrate how the API is
    called and we may not keep this in production.

--- apps/super-app/src/services/CmsSmokeTest.ts
+++ apps/super-app/src/services/CmsSmokeTest.ts
@@ -1,6 +1,7 @@
 // TODO This smoke test probably won't be useful in production. Reevaluate when
 // we start testing with real content types.
 const smokeTestEndpoint = '/api/smoke-test';
+const i18nSmokeTestEndpoint = '/api/smoke-test?locale=bn-BD';
 
 // TODO move this into a better module
 // Strapi wraps each model in a "data". Inside which, the things we care about
@@ -22,11 +23,27 @@ async function isUp(): Promise<boolean> {
   return !!json.data.attributes.test;
 }
 
+async function isI18nUp(): Promise<boolean> {
+  const response = await fetch(i18nSmokeTestEndpoint);
+  const json: StrapiModelResponse<SmokeTestView> = await response.json();
+  // Expect that the value we got is actually `true`.
+  return !!json.data.attributes.test;
+}
+
 export async function consoleSmokeTest() {
-  const up = await isUp();
+  // This unnecessarily waits for the slower promise to finish, but at least
+  // it's parallel, looks clean and the speed won't matter anyway.
+  const [up, i18nUp] = await Promise.all([isUp(), isI18nUp()]);
   if (up) {
     console.log('Connected to Strapi backend!!');
-    return;
+  } else {
+    console.error(
+      'Failed to connect to Strapi backend. Bad things might ensue.'
+    );
   }
-  console.error('Failed to connect to Strapi backend. Bad things might ensue.');
+  if (i18nUp) {
+    console.log('i18n enabled on Strapi!');
+  } else {
+    console.error('Failed to detect i18n on Strapi!');
+  }
 }
