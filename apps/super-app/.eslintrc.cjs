61c999091f5efaa6ba1320d581a847a127dc3835
Glen <glencbz@gmail.com>
2023-02-05 12:09:57 +0800

    super-app: prettier-ify config files

--- apps/super-app/.eslintrc.cjs
+++ apps/super-app/.eslintrc.cjs
@@ -4,10 +4,9 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     // Don't check formatting with eslint. This is less intrusive during dev,
     // e.g. eslint won't block you on badly formatted WIP
-    'prettier'
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
 };
-
