04f95ef5d2d45e78a5c307678f6b8c806d4e23e6
Glen <glencbz@gmail.com>
2023-02-26 17:28:46 -0800

    personal-calls/backend: add M series to FIN validation
    Since personal-calls/backend/src/util/VerifyFIN.ts was first written,
    ICA released the M FIN series. Hooray... Since M series is relatively
    new, the original source for the algorithm isn't complete any more, but
    at least the other two sources seem to agree.
    
    Refactor the verification code to make the checksum letters and offsets
    configurable, then update our verification algorithm to account for the
    M series, and add a test for each checksum letter. Each of the test
    cases were verified against the two FIN verifying services.

--- personal-calls/backend/src/util/VerifyFIN.test.ts
+++ personal-calls/backend/src/util/VerifyFIN.test.ts
@@ -27,3 +27,27 @@ test('More than 9 characters', () => {
 test('Numbers only without alphabet', () => {
   expect(validateFIN('123456789')).toBeFalsy();
 });
+
+test('M series', () => {
+  // One of each checksum letter
+  expect(validateFIN('M4627182X')).toBeTruthy();
+  expect(validateFIN('M1108730W')).toBeTruthy();
+  expect(validateFIN('M4503401U')).toBeTruthy();
+  expect(validateFIN('M4460557T')).toBeTruthy();
+  expect(validateFIN('M7986532R')).toBeTruthy();
+  expect(validateFIN('M1782041Q')).toBeTruthy();
+  expect(validateFIN('M2682514P')).toBeTruthy();
+  expect(validateFIN('M2966069N')).toBeTruthy();
+  expect(validateFIN('M2966078J')).toBeTruthy();
+  expect(validateFIN('M2083705L')).toBeTruthy();
+  expect(validateFIN('M1234567K')).toBeTruthy();
+
+  expect(validateFIN('M1234567X')).toBeFalsy();
+  expect(validateFIN('M4627182K')).toBeFalsy();
+  expect(validateFIN('M8967779T')).toBeFalsy();
+  expect(validateFIN('M4460557X')).toBeFalsy();
+  expect(validateFIN('M2966069P')).toBeFalsy();
+  expect(validateFIN('M2682514N')).toBeFalsy();
+  expect(validateFIN('M6198505L')).toBeFalsy();
+  expect(validateFIN('M2083705T')).toBeFalsy();
+});
