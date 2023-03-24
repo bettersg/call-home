eae729f7641c33e5a7058cdcb9ea7b17e3bb86ae
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2023-03-24 10:21:42 +0800

    Add HTTP(S) load balancer for GCS bucket (#145)
    * Add HTTP load balancer
    
    * Add compute engine API creation
    
    * Add resource for SSL certificate
    
    * Change HTTP proxy to HTTPS proxy
    
    * Update URL map resource name
    
    * Add depends_on for resources
    
    * Cleanup based on feedback
    
    * Move domain name to var
    
    * Add HTTP -> HTTPS redirect

--- infra/staging/main.tf
+++ infra/staging/main.tf
@@ -39,6 +39,11 @@ variable "frontend_bucket_name" {
   default = "call-home-frontend"
 }
 
+variable "website_domain_name" {
+  type = string
+  default = "app2-staging.callhome.sg"
+}
+
 ######################
 ## Project Settings ##
 ######################
@@ -150,6 +155,107 @@ resource "google_storage_bucket_iam_member" "frontend_bucket_viewer" {
   bucket = google_storage_bucket.call_home_frontend.name
   role   = "roles/storage.objectViewer"
   member = "allUsers"
+}
+
+#######################################
+## Load balancer for frontend bucket ##
+#######################################
+
+// Following the guide here: https://cloud.google.com/cdn/docs/setting-up-cdn-with-bucket
+
+// Enable Compute Engine API to setup an IP address
+resource "google_project_service" "compute_engine_api" {
+  project = data.google_project.project.project_id
+  service = "compute.googleapis.com"
+}
+
+// Setup IP address for load balancer
+resource "google_compute_global_address" "call_home_frontend_ip" {
+  project = data.google_project.project.project_id
+  name = "call-home-frontend-ip"
+}
+
+// Backend bucket with CDN policy with default ttl settings
+resource "google_compute_backend_bucket" "call_home_frontend_lb" {
+  name = "call-home-frontend-lb"
+  description = "Load balancer for the frontend GCS bucket"
+  project = data.google_project.project.project_id
+  bucket_name = google_storage_bucket.call_home_frontend.name
+  enable_cdn = true
+
+  cdn_policy {
+    cache_mode = "CACHE_ALL_STATIC"
+    client_ttl = 3600
+    default_ttl = 3600
+    max_ttl = 86400
+    negative_caching = true
+    serve_while_stale = 86400
+  }
+}
+
+// URL map for the backend bucket
+resource "google_compute_url_map" "call_home_lb" {
+  name = "call-home-lb"
+  project = data.google_project.project.project_id
+  default_service = google_compute_backend_bucket.call_home_frontend_lb.id
+}
+
+// Setup SSL certificate to enable HTTPS for load balancer
+resource "google_compute_managed_ssl_certificate" "call_home_frontend_lb_ssl" {
+  name = "call-home-frontend-lb-ssl"
+  project = data.google_project.project.project_id
+
+  managed {
+    domains = [var.website_domain_name]
+  }
+}
+
+// HTTPS proxy
+resource "google_compute_target_https_proxy" "call_home_frontend_lb_proxy" {
+  name = "call-home-frontend-lb-proxy"
+  project = data.google_project.project.project_id
+  url_map = google_compute_url_map.call_home_lb.id
+
+  ssl_certificates = [
+    google_compute_managed_ssl_certificate.call_home_frontend_lb_ssl.name
+  ]
+}
+
+// Forwarding rule
+resource "google_compute_global_forwarding_rule" "call_home_frontend_lb_forwarding" {
+  name = "call-home-frontend-lb-forwarding"
+  project = data.google_project.project.project_id
+  ip_protocol = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range = "443"
+  target = google_compute_target_https_proxy.call_home_frontend_lb_proxy.id
+  ip_address = google_compute_global_address.call_home_frontend_ip.id
+}
+
+// The following resources create a partial load balancer
+// for redirecting HTTP to HTTPS
+resource "google_compute_url_map" "call_home_lb_http_redirect" {
+  name = "call-home-lb-http-redirect"
+  project = data.google_project.project.project_id
+
+  default_url_redirect {
+    strip_query            = false
+    https_redirect         = true
+  }
+}
+
+resource "google_compute_target_http_proxy" "call_home_lb_http_redirect" {
+  name    = "call-home-lb-http-redirect-proxy"
+  project = data.google_project.project.project_id
+  url_map = google_compute_url_map.call_home_lb_http_redirect.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "call_home_lb_http_redirect" {
+  name       = "call-home-lb-http-redirect-forwarding-rule"
+  project = data.google_project.project.project_id
+  target     = google_compute_target_http_proxy.call_home_lb_http_redirect.self_link
+  ip_address = google_compute_global_address.call_home_frontend_ip.id
+  port_range = "80"
 }
 
 #############
